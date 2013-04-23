9eebadceced6a3c25a4c43d02bf35a1276d522f7
Manuel Reinhardt <reinhardt@syslab.com>
2013-04-23 15:19:57 +0100

    moved actual dotfiles to src dir, removed explicit list from install.py

--- install.py
+++ install.py
@@ -2,10 +2,9 @@
 import os
 import sys
 
-dotfiles = ['.bashrc', '.vimrc', ]
-
 
 def install(source_dir, install_dir, backup_dir):
+    dotfiles = os.listdir(source_dir)
     if not os.path.exists(backup_dir):
         os.mkdir(backup_dir)
     for dotfile in dotfiles:
@@ -31,6 +30,7 @@ def install(source_dir, install_dir, backup_dir):
 
 
 def uninstall(source_dir, install_dir, backup_dir):
+    dotfiles = os.listdir(source_dir)
     if not os.path.exists(backup_dir):
         sys.exit('Backup dir not found (%s), aborting' % backup_dir)
     for dotfile in dotfiles:
@@ -51,6 +51,7 @@ def uninstall(source_dir, install_dir, backup_dir):
     except OSError:
         print('Could not delete backup dir, not empty? (%s)' % backup_dir)
 
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         install_dir = sys.argv[1]
@@ -58,10 +59,11 @@ if __name__ == '__main__':
         install_dir = os.path.expanduser('~')
     backup_dir = os.path.join(os.path.expanduser('~'), 'dotfiles.old')
     here_path = os.path.abspath(__file__)
-    here_folder = os.path.split(here_path)[0]
+    here_dir = os.path.split(here_path)[0]
+    source_dir = os.path.join(here_dir, 'src')
     call_name = os.path.split(sys.argv[0])[1]
     if call_name == 'install.py':
-        install(here_folder, install_dir, backup_dir)
+        install(source_dir, install_dir, backup_dir)
     elif call_name == 'uninstall.py':
-        uninstall(here_folder, install_dir, backup_dir)
+        uninstall(source_dir, install_dir, backup_dir)
 
