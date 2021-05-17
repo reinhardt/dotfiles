e660a4ae07956f1e4f15afcfc229d4d83ab8510e
Manuel Reinhardt <reinhardt@syslab.com>
2021-05-17 15:06:20 +0200

    dunst-script.py: Black; check for mattermost.

--- src/.local/bin/dunst-script.py
+++ src/.local/bin/dunst-script.py
@@ -5,12 +5,9 @@ import sys
 
 _, appname, summary, body, icon, urgency = sys.argv
 
-if 'Tea' in summary:
-    subprocess.call(['aplay /home/reinhardt/Sounds/MW2/HEATLEVL.WAV'],
-                    shell=True)
-elif ' wrote: ' in body:
-    subprocess.call(['aplay /home/reinhardt/Sounds/MW2/inmessage.wav'],
-                    shell=True)
-elif summary == 'Battery critical':
-    subprocess.call(['aplay /home/reinhardt/Sounds/MW2/crtihit.wav'],
-                    shell=True)
+if "Tea" in summary:
+    subprocess.call(["aplay /home/reinhardt/Sounds/MW2/HEATLEVL.WAV"], shell=True)
+elif " wrote: " in body or "mattermost" in body:
+    subprocess.call(["aplay /home/reinhardt/Sounds/MW2/inmessage.wav"], shell=True)
+elif summary == "Battery critical":
+    subprocess.call(["aplay /home/reinhardt/Sounds/MW2/crtihit.wav"], shell=True)
